bd6840ff4c98f0f01fd68f5a119dcbb7a47acfd8
Laxman Bista <lax.bista@gmail.com>
2021-02-17 14:30:50 -0700

    Add files via upload

---
+++ filesIO/q1.py
@@ -0,0 +1,8 @@
+# Exercise Question 1: Accept two numbers from the user and calculate multiplication
+
+num1 = int(input('First Number: '))
+num2 = int(input('Second Number: '))
+
+multiply = num1*num2
+
+print(multiply)

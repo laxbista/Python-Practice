2c96693ee30756e566cf18f562f08043c1535961
Laxman <lax.bista@gmail.com>
2021-02-17 14:45:33 -0700

    Solution

---
+++ filesIO_Excercise/q2.py
@@ -0,0 +1,5 @@
+# Exercise Question 2: Display “My Name Is James”
+# as “My**Name**Is**James” using output formatting of a print() function
+
+# print('My' + '**' + 'Name'+ '**' + 'Is'+ '**' + 'Lax')
+print('My', 'Name', 'Is', 'Lax', sep='**')
